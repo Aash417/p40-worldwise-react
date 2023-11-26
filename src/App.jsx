e6fbd86c93984217d7f88874fead010129895ae2
aashish_k <aashishkathat00@gmail.com>
2023-11-26 20:07:33 +0530

    implemented main pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-function App() {
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Product from './pages/Product';
+import Pricing from './pages/Pricing';
+import Homepage from './pages/Homepage';
+import PageNotFound from './pages/PageNotFound';
 
-    const x =9;
-    return (
-        <div>
-            World wise
-        </div>
-    )
+function App() {
+  return (
+    <div>
+      <h2>hello</h2>
+      <BrowserRouter>
+        <Routes>
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="/" element={<Homepage />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
