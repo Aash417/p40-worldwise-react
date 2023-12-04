4ec0b68e34ff1c10eaaf362dd26bd8658e7bb69a
aashish_k <aashishkathat00@gmail.com>
2023-12-05 01:45:37 +0530

    creating a new city

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,8 @@
 // "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
 
 import { useEffect, useState } from 'react';
+import ReactDatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 import styles from './Form.module.css';
 import Button from './Button';
@@ -8,103 +10,130 @@ import BackButton from './BackButton';
 import useUrlPosition from '../hooks/useUrlPosition';
 import Message from './Message';
 import Spinner from './Spinner';
+import { useCities } from '../contexts/CitiesContext';
+import { useNavigate } from 'react-router-dom';
 
 function convertToEmoji(countryCode) {
-    const codePoints = countryCode
-        .toUpperCase()
-        .split('')
-        .map((char) => 127397 + char.charCodeAt());
-    // console.log(String.fromCodePoint(...codePoints));
-    return String.fromCodePoint(...codePoints);
+   const codePoints = countryCode
+      .toUpperCase()
+      .split('')
+      .map((char) => 127397 + char.charCodeAt());
+   // console.log(String.fromCodePoint(...codePoints));
+   return String.fromCodePoint(...codePoints);
 }
 
 const BASE_URL = `https://api.bigdatacloud.net/data/reverse-geocode-client`;
 
 function Form() {
-    const [isLoadingGeocoding, setIsLoadingGeocoding] = useState(false);
-    const [lat, lng] = useUrlPosition();
-    const [cityName, setCityName] = useState('');
-    const [country, setCountry] = useState('');
-    const [date, setDate] = useState(new Date());
-    const [notes, setNotes] = useState('');
-    const [emoji, setEmoji] = useState('');
-    const [geocodingError, setGeocodingError] = useState('');
+   const [lat, lng] = useUrlPosition();
+   const { createCity, isLoading } = useCities();
+   const navigate = useNavigate();
 
-    useEffect(
-        function () {
-            async function fetchCityData() {
-                try {
-                    setIsLoadingGeocoding(true);
-                    setGeocodingError('');
-                    console.log(lat, lng);
-                    const res = await fetch(
-                        // `${BASE_URL}?latitude=${lat}&=longitude=${lng}`
-                        `${BASE_URL}?latitude=${lat}&longitude=${lng}`
-                    );
-                    const data = await res.json();
-                    console.log(data);
+   const [isLoadingGeocoding, setIsLoadingGeocoding] = useState(false);
+   const [cityName, setCityName] = useState('');
+   const [country, setCountry] = useState('');
+   const [date, setDate] = useState(new Date());
+   const [notes, setNotes] = useState('');
+   const [emoji, setEmoji] = useState('');
+   const [geocodingError, setGeocodingError] = useState('');
 
-                    if (!data.countryCode)
-                        throw new Error(
-                            'there doesnt seem to be a city , click somewhere else pls'
-                        );
+   useEffect(
+      function () {
+         if (!lat && !lng) return;
+         async function fetchCityData() {
+            try {
+               setIsLoadingGeocoding(true);
+               setGeocodingError('');
+               const res = await fetch(
+                  // `${BASE_URL}?latitude=${lat}&=longitude=${lng}`
+                  `${BASE_URL}?latitude=${lat}&longitude=${lng}`
+               );
+               const data = await res.json();
 
-                    setCityName(data.city || data.locality || '');
-                    setCountry(data.countryName);
-                    setEmoji(convertToEmoji(data.countryCode));
-                } catch (error) {
-                    console.error(error);
-                    setGeocodingError(error.message);
-                } finally {
-                    setIsLoadingGeocoding(false);
-                }
+               if (!data.countryCode)
+                  throw new Error(
+                     'there doesnt seem to be a city , click somewhere else pls'
+                  );
+
+               setCityName(data.city || data.locality || '');
+               setCountry(data.countryName);
+               setEmoji(convertToEmoji(data.countryCode));
+            } catch (error) {
+               console.error(error);
+               setGeocodingError(error.message);
+            } finally {
+               setIsLoadingGeocoding(false);
             }
+         }
 
-            fetchCityData();
-        },
-        [lat, lng]
-    );
-    if (isLoadingGeocoding) return <Spinner />;
-    if (geocodingError) return <Message message={geocodingError} />;
+         fetchCityData();
+      },
+      [lat, lng]
+   );
 
-    return (
-        <form className={styles.form}>
-            <div className={styles.row}>
-                <label htmlFor="cityName">City name</label>
-                <input
-                    id="cityName"
-                    onChange={(e) => setCityName(e.target.value)}
-                    value={cityName}
-                />
-                <span className={styles.flag}>{emoji}</span>
-            </div>
+   async function handleSubmit(e) {
+      e.preventDefault();
 
-            <div className={styles.row}>
-                <label htmlFor="date">When did you go to {cityName}?</label>
-                <input
-                    id="date"
-                    onChange={(e) => setDate(e.target.value)}
-                    value={date}
-                />
-            </div>
+      if (!cityName || !date) return;
 
-            <div className={styles.row}>
-                <label htmlFor="notes">
-                    Notes about your trip to {cityName}
-                </label>
-                <textarea
-                    id="notes"
-                    onChange={(e) => setNotes(e.target.value)}
-                    value={notes}
-                />
-            </div>
+      const newCity = {
+         cityName,
+         country,
+         emoji,
+         date,
+         notes,
+         position: { lat, lng }
+      };
+      // console.log(newCity);
+      await createCity(newCity);
+      navigate('/app/cities');
+   }
 
-            <div className={styles.buttons}>
-                <Button type="primary">Add</Button>
-                <BackButton />
-            </div>
-        </form>
-    );
+   if (isLoadingGeocoding) return <Spinner />;
+   if (!lat && !lng) return <Message message="start by clicking on the map" />;
+   if (geocodingError) return <Message message={geocodingError} />;
+
+   return (
+      <form
+         className={`${styles.form} ${isLoading ? styles.loading : ''}`}
+         onSubmit={handleSubmit}
+      >
+         <div className={styles.row}>
+            <label htmlFor="cityName">City name</label>
+            <input
+               id="cityName"
+               onChange={(e) => setCityName(e.target.value)}
+               value={cityName}
+            />
+            <span className={styles.flag}>{emoji}</span>
+         </div>
+
+         <div className={styles.row}>
+            <label htmlFor="date">When did you go to {cityName}?</label>
+            <ReactDatePicker
+               id="date"
+               value={date}
+               onChange={(date) => setDate(date)}
+               selected={date}
+               dateFormat="dd/MM/yyyy"
+            ></ReactDatePicker>
+         </div>
+
+         <div className={styles.row}>
+            <label htmlFor="notes">Notes about your trip to {cityName}</label>
+            <textarea
+               id="notes"
+               onChange={(e) => setNotes(e.target.value)}
+               value={notes}
+            />
+         </div>
+
+         <div className={styles.buttons}>
+            <Button type="primary">Add</Button>
+            <BackButton />
+         </div>
+      </form>
+   );
 }
 
 export default Form;
--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -4,58 +4,79 @@ const BASE_URL = 'http://localhost:7999';
 const CitiesContext = createContext();
 
 function CitiesProvider({ children }) {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [currentCity, setCurrentCity] = useState({});
+   const [cities, setCities] = useState([]);
+   const [isLoading, setIsLoading] = useState(false);
+   const [currentCity, setCurrentCity] = useState({});
 
-  useEffect(function () {
-    async function fetchCities() {
-      try {
-        setIsLoading(true);
-        const res = await fetch(`${BASE_URL}/cities`);
-        const data = await res.json();
-        setCities(data);
-      } catch (error) {
-        alert('There was an error loading data.');
-      } finally {
-        setIsLoading(false);
+   useEffect(function () {
+      async function fetchCities() {
+         try {
+            setIsLoading(true);
+            const res = await fetch(`${BASE_URL}/cities`);
+            const data = await res.json();
+            setCities(data);
+         } catch (error) {
+            alert('There was an error loading data.');
+         } finally {
+            setIsLoading(false);
+         }
       }
-    }
-    fetchCities();
-  }, []);
+      fetchCities();
+   }, []);
 
-  async function getCity(id) {
-    try {
-      setIsLoading(true);
-      const res = await fetch(`${BASE_URL}/cities/${id}`);
-      const data = await res.json();
-      setCurrentCity(data);
-    } catch (error) {
-      alert('There was an error loading data.');
-    } finally {
-      setIsLoading(false);
-    }
-  }
+   async function getCity(id) {
+      try {
+         setIsLoading(true);
+         const res = await fetch(`${BASE_URL}/cities/${id}`);
+         const data = await res.json();
+         setCurrentCity(data);
+      } catch (error) {
+         alert('There was an error loading data.');
+      } finally {
+         setIsLoading(false);
+      }
+   }
+   async function createCity(newCity) {
+      try {
+         setIsLoading(true);
+         const res = await fetch(`${BASE_URL}/cities`, {
+            method: 'POST',
+            body: JSON.stringify(newCity),
+            headers: {
+               'content-type': 'application/json'
+            }
+         });
+         const data = await res.json();
+         setCities((cities) => [...cities, data]);
 
-  return (
-    <CitiesContext.Provider
-      value={{
-        cities,
-        isLoading,
-        currentCity,
-        getCity,
-      }}
-    >
-      {children}
-    </CitiesContext.Provider>
-  );
+         console.log(data);
+      } catch (error) {
+         alert('There was an error loading data.');
+      } finally {
+         setIsLoading(false);
+      }
+   }
+
+   return (
+      <CitiesContext.Provider
+         value={{
+            cities,
+            isLoading,
+            currentCity,
+            getCity,
+            createCity
+         }}
+      >
+         {children}
+      </CitiesContext.Provider>
+   );
 }
 function useCities() {
-  const context = useContext(CitiesContext);
+   const context = useContext(CitiesContext);
 
-  if (context === undefined)
-    throw new Error('context used outside citiesProvider');
+   if (context === undefined)
+      throw new Error('context used outside citiesProvider');
 
-  return context;
+   return context;
 }
 export { CitiesProvider, useCities };
