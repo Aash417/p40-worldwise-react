b473f18f8c71a527268965e057eeba62e08b0ffe
aashish_k <aashishkathat00@gmail.com>
2023-12-07 00:21:27 +0530

    implemnting login page

--- src/App.jsx
+++ src/App.jsx
@@ -11,30 +11,39 @@ import CountryList from './components/CountryList';
 import City from './components/City';
 import Form from './components/Form';
 import { CitiesProvider } from './contexts/CitiesContext';
+import { AuthProvider } from './contexts/FakeAuthContext';
 
 function App() {
-  return (
-    <div>
-      <CitiesProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route index element={<Homepage />} />
-            <Route path="product" element={<Product />} />
-            <Route path="pricing" element={<Pricing />} />
-            <Route path="login" element={<Login />} />
-            <Route path="app" element={<Applayout />}>
-              <Route index element={<Navigate replace to="cities" />}></Route>
-              <Route path="cities" element={<CityList />}></Route>
-              <Route path="cities/:id" element={<City />} />
-              <Route path="countries" element={<CountryList />}></Route>
-              <Route path="form" element={<Form />}></Route>
-            </Route>
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </CitiesProvider>
-    </div>
-  );
+   return (
+      <div>
+         <AuthProvider>
+            <CitiesProvider>
+               <BrowserRouter>
+                  <Routes>
+                     <Route index element={<Homepage />} />
+                     <Route path="product" element={<Product />} />
+                     <Route path="pricing" element={<Pricing />} />
+                     <Route path="login" element={<Login />} />
+                     <Route path="app" element={<Applayout />}>
+                        <Route
+                           index
+                           element={<Navigate replace to="cities" />}
+                        ></Route>
+                        <Route path="cities" element={<CityList />}></Route>
+                        <Route path="cities/:id" element={<City />} />
+                        <Route
+                           path="countries"
+                           element={<CountryList />}
+                        ></Route>
+                        <Route path="form" element={<Form />}></Route>
+                     </Route>
+                     <Route path="*" element={<PageNotFound />} />
+                  </Routes>
+               </BrowserRouter>
+            </CitiesProvider>
+         </AuthProvider>
+      </div>
+   );
 }
 
 export default App;
